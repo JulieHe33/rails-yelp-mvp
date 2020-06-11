bc702b7596342bd6f1818ebef214521953b587aa
Julie Henni <henni.julie@gmail.com>
2020-06-11 11:06:44 +0200

    exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create([{ name: 'Opus V', address: 'Mannheim', category: 'chinese' }])
+Restaurant.create([{ name: 'Die Zeder', address: 'Ludwigshafen', category: 'belgian' }])
+Restaurant.create([{ name: 'Grill Royal', address: 'Berlin', category: 'italian' }])
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
+
+[ dishoom, pizza_east ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
